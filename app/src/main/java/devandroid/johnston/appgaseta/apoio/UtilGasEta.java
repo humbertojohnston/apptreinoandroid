8cc74a3967b0727f4ccd30dbe4a7199493e74751
humbertojohnston <hjpinheirojr@gmail.com>
2023-12-06 16:13:17 -0300

    Aula 57 - Método Estático para calcular o melhor preço

--- app/src/main/java/devandroid/johnston/appgaseta/apoio/UtilGasEta.java
+++ app/src/main/java/devandroid/johnston/appgaseta/apoio/UtilGasEta.java
@@ -2,13 +2,43 @@ package devandroid.johnston.appgaseta.apoio;
 
 public class UtilGasEta {
 
-    public void metodoNãoEstatico() {}
+    public void metodoEstatico() {}
 
-    public static void metodoEstatico (){}
+    public static void metodoNaoEstatico (){}
 
     public static String mensagem () {
 
         return "Qualquer mensagem...";
+    }
+
+    public static double preco(){
+
+        return 9.99;
+    }
+
+    public static String calcularMelhorOpcao(double gasolina, double etanol) {
+
+        // PREÇO DA GASOLINA: R$ 5.12
+        // PREÇO DO ETANOL: R$ 3.99
+        //PREÇO IDEAL: GASOLINA * 0.70 = R$ 3.548
+
+        double precoIdeal = gasolina * 0.70;
+        String mensagemDeRetorno;
+
+        if(etanol <= precoIdeal) {
+
+            mensagemDeRetorno = "Abastecer com Etanol";
+
+        } else {
+
+          mensagemDeRetorno = "Abastecer com Gasolina";
+        }
+
+        //SE O PREÇO DO ETANOL FOR IGUAL OU MENOR QUE O PREÇO IDEAL,
+        //MELHOR ABASTECER COM ETANOL, CASO CONTRÁRIO, A GASOLINA É
+        //MAIS VANTAGEM.
+
+        return mensagemDeRetorno; //ABASTECER COM GASOLINA OU ABASTECER COM ETANOL
     }
 
 }
--- app/src/main/java/devandroid/johnston/appgaseta/view/GasEtaActivity.java
+++ app/src/main/java/devandroid/johnston/appgaseta/view/GasEtaActivity.java
@@ -13,18 +13,19 @@ public class GasEtaActivity extends AppCompatActivity {
 
     //UtilGasEta utilGasEta = new UtilGasEta();
 
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_gaseta); //APRESENTA O LAYOUT NA TELA
 
-       // utilGasEta.metodoNaoEstatico();
+        UtilGasEta.metodoNaoEstatico();
 
        //UtilGasEta.metodoEstatico();
 
-        Toast.makeText(GasEtaActivity.this, UtilGasEta.mensagem(),
+        Toast.makeText(GasEtaActivity.this,
+                UtilGasEta.calcularMelhorOpcao(5.12, 3.39),
                 Toast.LENGTH_LONG).show();
     }
+
 }
