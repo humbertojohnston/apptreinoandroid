c22d05d6a5f9cccea5af2d23fdd7c4463a14d928
humbertojohnston <hjpinheirojr@gmail.com>
2023-12-07 20:02:22 -0300

    Aulas 59, 60 e 61 - Implementação dos botões LIMPAR, CALCULAR E SALVAR

--- app/src/main/java/devandroid/johnston/appgaseta/view/GasEtaActivity.java
+++ app/src/main/java/devandroid/johnston/appgaseta/view/GasEtaActivity.java
@@ -1,6 +1,7 @@
 package devandroid.johnston.appgaseta.view;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,8 +13,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import devandroid.johnston.appgaseta.R;
 import devandroid.johnston.appgaseta.apoio.UtilGasEta;
+import devandroid.johnston.appgaseta.model.Combustivel;
 
 public class GasEtaActivity extends AppCompatActivity {
+
+    Combustivel combustivelGasolina;
+    Combustivel combustivelEtanol;
 
     //DECLARAÇÃO DOS ATRIBUTOS
     EditText editGasolina;
@@ -25,6 +30,11 @@ public class GasEtaActivity extends AppCompatActivity {
     Button btnLimpar;
     Button btnSalvar;
     Button btnFinalizar;
+
+    double precoGasolina;
+    double precoEtanol;
+    String recomendacao;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -47,7 +57,37 @@ public class GasEtaActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                boolean isDadosOk = true;
 
+                if(TextUtils.isEmpty(editGasolina.getText())) {
+                    editGasolina.setError("* CAMPO NÃO PODE FICAR EM BRANCO");
+                    editGasolina.requestFocus(); //VOLTA O CURSOR PARA O CAMPO
+                    isDadosOk = false;
+
+                }
+
+                if(TextUtils.isEmpty(editEtanol.getText())) {
+                    editEtanol.setError("* CAMPO NÃO PODE FICAR EM BRANCO");
+                    editEtanol.requestFocus(); //VOLTA O CURSOR PARA O CAMPO
+                    isDadosOk = false;
+
+                }
+
+                if (isDadosOk){
+
+                    precoGasolina = Double.parseDouble(editGasolina.getText().toString());//para poder colocar um double dentro de uma String
+                    precoEtanol = Double.parseDouble(editEtanol.getText().toString());//para poder colocar um double dentro de uma String
+
+                    recomendacao = UtilGasEta.calcularMelhorOpcao(precoGasolina,precoEtanol);
+
+                    txtResultado.setText(recomendacao);
+
+                } else {
+
+                    Toast.makeText(GasEtaActivity.this,
+                        "* CAMPOS DE PREENCHIMENTO OBRIGATÓRIO *", Toast.LENGTH_LONG).show();
+
+                }
             }
 
         });
@@ -56,12 +96,31 @@ public class GasEtaActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
+                editEtanol.setText("");
+                editGasolina.setText("");
+
             }
         });
 
         btnSalvar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                //TODO: DESABILITAR O BOTÃO SALVAR
+
+                combustivelGasolina = new Combustivel();
+                combustivelEtanol = new Combustivel();
+
+                combustivelGasolina.setNomeDoCombustivel("Gasolina");
+                combustivelGasolina.setPrecoDoCombustivel(precoGasolina);
+
+                combustivelEtanol.setNomeDoCombustivel("Etanol");
+                combustivelEtanol.setPrecoDoCombustivel(precoEtanol);
+
+                combustivelGasolina.setRecomendacao(UtilGasEta.calcularMelhorOpcao(precoGasolina, precoEtanol));
+                combustivelEtanol.setRecomendacao(UtilGasEta.calcularMelhorOpcao(precoGasolina,precoEtanol));
+
+                int parada = 0;
 
             }
         });
@@ -70,7 +129,9 @@ public class GasEtaActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Toast.makeText(GasEtaActivity.this, "* PROGRAMA FINALIZADO *", Toast.LENGTH_LONG).show();
+                Toast.makeText(GasEtaActivity.this, "* PROGRAMA FINALIZADO *",
+                        Toast.LENGTH_LONG).show();
+
                 finish();
 
             }
